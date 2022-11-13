811632bee2818c82018b55547608b39f92f8f562
Jean_Sky_Poor <katovano2705@gmail.com>
2022-11-14 01:21:01 +0300

    add def to read data for table

--- secondary_defs.py
+++ secondary_defs.py
@@ -1,7 +1,7 @@
-import psycopg2
-from config import *
-import pytest
-
-
-
-
+def read_data_for_table():
+    with open('data.txt', 'r') as f:
+            fast_list=[]
+            for line in f:
+                fast_list.append(line.strip('\n'))
+            
+            return fast_list
