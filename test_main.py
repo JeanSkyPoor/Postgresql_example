979e1b984df0c9e7d58caab73a233abef79577f5
Jean_Sky_Poor <katovano2705@gmail.com>
2022-11-13 23:21:21 +0300

    add create_table

--- test_main.py
+++ test_main.py
@@ -1,29 +1,14 @@
 from config import *
-import psycopg2
 
 
-try:
-    #connect to exist database
-    connection = psycopg2.connect(
-        host = host,
-        user = user,
-        password = password,
-        database = database
-    )
 
-    #the cursor for performing database operations
+def test_create_mobile_table(connection):
     with connection.cursor() as cursor:
         cursor.execute(
-           "SELECT version()" 
-        )
-        print(f"server version is {cursor.fetchone()}")
-
-
-except Exception as _ex:
-    print("[INFO] Error while working with PostgreSQL", _ex)
-finally:
-    if connection:
-        connection.close()
-        print("[INFO] PostgreSQL connection closed")
+                """CREATE TABLE mobiles (
+                    id serial PRIMARY KEY,
+                    name VARCHAR(50)
+                );"""
+            )
 
 
